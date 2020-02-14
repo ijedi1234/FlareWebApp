b2e8cfdb1bcf1110140c8d064aaa40af3e413743
lludko2 <lludko2@uic.edu>
2020-02-14 13:41:04 -0600

    Added test case and working file upload + response

--- FlareWebApp/Controllers/HomeController.cs
+++ FlareWebApp/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FlareWebApp.Models;
+using FlareWebApp.FileLogic;
+//using System.Web.UI.WebControls;
 
 namespace FlareWebApp.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
+            HomePageModel model = new HomePageModel();
+            return View(model);
         }
 
         public ActionResult About() {
@@ -20,6 +24,15 @@ namespace FlareWebApp.Controllers {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(HomePageModel model) {
+            string savePath = HttpContext.Server.MapPath("~/SavedData");
+            FileSaver.SaveFile(model.SomeFile, savePath);
+            model.FreqWords = new List<string>();
+            model.FreqWords.Add("Example");
+            return View("Index", model);
         }
     }
 }
